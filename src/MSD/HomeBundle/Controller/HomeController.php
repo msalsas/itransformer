288692e064e7c807eeef6a1c3b8fc567118a8bd9
msalsas <manolez@gmail.com>
2013-12-11 18:55:50 +0100

    fixed rotate action and changed frame images

--- src/MSD/HomeBundle/Controller/HomeController.php
+++ src/MSD/HomeBundle/Controller/HomeController.php
@@ -289,8 +289,8 @@ class HomeController extends Controller
 				if(isset($_POST['rotacion']) && is_numeric($_POST['rotacion']) && $_POST['rotacion']<=360 && $_POST['rotacion']>=0 ) {	
 					//cambiar las dimensiones de la imagen
 									
-					$x = floor(($imagen->getAlto() * cos(deg2rad(90 - $_POST['rotacion']))) + abs($imagen->getAncho() * cos(deg2rad($_POST['rotacion']))))-2;
-					$y = floor(($imagen->getAlto() * cos(deg2rad($_POST['rotacion']))) + abs($imagen->getAncho() * cos(deg2rad(90 - $_POST['rotacion']))))-2;
+					$x = abs(floor(($imagen->getAlto() * cos(deg2rad(90 - $_POST['rotacion']))) + abs($imagen->getAncho() * cos(deg2rad($_POST['rotacion']))))-2);
+					$y = abs(floor(($imagen->getAlto() * cos(deg2rad($_POST['rotacion']))) + abs($imagen->getAncho() * cos(deg2rad(90 - $_POST['rotacion']))))-2);
 					$imagen->setAlto($y);
 					$imagen->setAncho($x);
 					if($lienzo = imagerotate ($lienzo, $_POST['rotacion'], 0)){
