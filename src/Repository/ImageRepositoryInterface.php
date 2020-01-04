4a8e43dd1bba2313eaf5385213bc56ddae6c188f
msalsas <manolez@gmail.com>
2020-01-04 11:04:34 +0100

    Fix image repository interface

--- src/Repository/ImageRepositoryInterface.php
+++ src/Repository/ImageRepositoryInterface.php
@@ -6,6 +6,6 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepositoryInterface;
 
 interface ImageRepositoryInterface extends ServiceEntityRepositoryInterface
 {
-    public function find($id, $lockMode, $lockVersion);
-    public function findByName($id);
+    public function find($id, $lockMode = null, $lockVersion = null);
+    public function findByName($name);
 }
