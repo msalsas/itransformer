e744a97224e307c959885b66c8421e508be6617b
msalsas <manolez@gmail.com>
2020-01-05 17:38:18 +0100

    Small fixes

--- assets/js/imageUpload.js
+++ assets/js/imageUpload.js
@@ -24,102 +24,124 @@ const ajaxSubmit = require('jquery-form');
 
 $(function() {
     var form = $('form[name="image"]')[0];
-    form.reset();
-    var $form = $(form);
 
-    var tam_max = 3145728;
-    var $fileInput = $form.find('input[type="file"]');
-    var _self = this;
-    $fileInput.bind('change', function(e) {
-        e.preventDefault();
-        var files = this.files;
+    if (form) {
+        form.reset();
+        var $form = $(form);
 
-        var $file = files === "undefined" ? $fileInput : files[0];
-        console.log($file);
-        if (!isValid($file, $fileInput)) {
-            submit($form);
-            showError($file, $fileInput);
-        } else {
-            submit($form);
-        }
+        var tam_max = 3145728;
+        var $fileInput = $form.find('input[type="file"]');
+        var _self = this;
+        $fileInput.bind('change', function (e) {
+            e.preventDefault();
+            var files = this.files;
 
-        function submit($form) {
-            var httpRequest;
-            if (window.XMLHttpRequest) {
-                httpRequest = new XMLHttpRequest();
-            } else if (window.ActiveXObject) {
-                try {
-                    httpRequest = new ActiveXObject("MSXML2.XMLHTTP");
-                } catch (e) {
+            var $file = files === "undefined" ? $fileInput : files[0];
+            console.log($file);
+            if (!isValid($file, $fileInput)) {
+                submit($form);
+                showError($file, $fileInput);
+            } else {
+                submit($form);
+            }
+
+            function submit($form) {
+                var httpRequest;
+                if (window.XMLHttpRequest) {
+                    httpRequest = new XMLHttpRequest();
+                } else if (window.ActiveXObject) {
                     try {
-                        httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-                    } catch (e) {}
+                        httpRequest = new ActiveXObject("MSXML2.XMLHTTP");
+                    } catch (e) {
+                        try {
+                            httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                        } catch (e) {
+                        }
+                    }
+                }
+                if (!httpRequest) {
+                    $form.submit();
+                } else {
+                    var bar = $('.bar');
+                    var percent = $('.percent');
+                    var status = $('#status');
+
+                    $form.ajaxSubmit({
+                        beforeSend: function () {
+                            status.empty();
+                            var percentVal = '0%';
+                            bar.width(percentVal)
+                            percent.html(percentVal);
+                            $(".in_file").hide();
+                            $(".progress").after('<div class="wait">Wait...</div>')
+                        },
+                        uploadProgress: function (event, position, total, percentComplete) {
+                            var percentVal = percentComplete + '%';
+                            bar.width(percentVal)
+                            percent.html(percentVal);
+                        },
+                        success: function () {
+                            var percentVal = '100%';
+                            bar.width(percentVal)
+                            percent.html(percentVal);
+                        },
+                        complete: function (xhr) {
+                            $('#agrupar').after(xhr.responseText);
+                            $('#img_cargada').show()
+                            $(".wait").remove();
+                            window.scrollTo(0, 0);
+                        }
+                    });
+
                 }
             }
-            if (!httpRequest) {
-                $form.submit();
-            } else {
-                var bar = $('.bar');
-                var percent = $('.percent');
-                var status = $('#status');
 
-                $form.ajaxSubmit({
-                    beforeSend: function() {
-                        status.empty();
-                        var percentVal = '0%';
-                        bar.width(percentVal)
-                        percent.html(percentVal);
-                        $(".in_file").hide();
-                        $(".progress").after('<div class="wait">Wait...</div>')
-                    },
-                    uploadProgress: function(event, position, total, percentComplete) {
-                        var percentVal = percentComplete + '%';
-                        bar.width(percentVal)
-                        percent.html(percentVal);
-                    },
-                    success: function() {
-                        var percentVal = '100%';
-                        bar.width(percentVal)
-                        percent.html(percentVal);
-                    },
-                    complete: function(xhr) {
-                        $('#agrupar').after(xhr.responseText);
-                        $('#img_cargada').show()
-                        $(".wait").remove();
-                        window.scrollTo(0,0);
-                    }
-                });
-
-            }
-        }
-
-        function isValid($file, $fileInput) {
-            return $file.size <= tam_max && $fileInput.val().match('.+\\.(jpe?g|png|wbmp|gif|JPE?G|PNG|WBMP|GIF)$');
-        }
-
-        function showError($file, $fileInput) {
-            var error = '';
-
-            if($file.size > tam_max){
-                error += '\nFile size: ' + $fileInput.size + ' Bytes. Too large. MAX: '+ tam_max + ' Bytes. \n';
+            function isValid($file, $fileInput) {
+                return $file.size <= tam_max && $fileInput.val().match('.+\\.(jpe?g|png|wbmp|gif|JPE?G|PNG|WBMP|GIF)$');
             }
 
-            //if($file.val().match('.+\\.(jpe?g|png|wbmp|gif|JPE?G|PNG|WBMP|GIF)$')==null){
+            function showError($file, $fileInput) {
+                var error = '';
+
+                if ($file.size > tam_max) {
+                    error += '\nFile size: ' + $fileInput.size + ' Bytes. Too large. MAX: ' + tam_max + ' Bytes. \n';
+                }
+
+                //if($file.val().match('.+\\.(jpe?g|png|wbmp|gif|JPE?G|PNG|WBMP|GIF)$')==null){
                 error += '\nFormat not accepted. Accepted formats: jpg, jpeg, png, wbmp and gif.';
-            //}
+                //}
 
-alert(error)
-            window.scrollTo(0,0);
-            var htmlError = '<div id="todo"></div><div id="botones"><div id="error">'+error+'</div><input value="OK" id="volver" style="width: 20%" onclick="$(\'#todo\').remove(); $(\'#botones\').remove();"></div>';
+                alert(error)
+                window.scrollTo(0, 0);
+                var htmlError = '<div id="todo"></div><div id="botones"><div id="error">' + error + '</div><input value="OK" id="volver" style="width: 20%" onclick="$(\'#todo\').remove(); $(\'#botones\').remove();"></div>';
 
-            $('#agrupar').after(htmlError);
-            $("#botones").css({position: "absolute", top: "2%", left: "15%", width: "60%", marginTop: "0", marginLeft: "0", fontSize: "1.5em", border: "1px solid #999999", backgroundColor: "rgba(200,200,200,0.5)", padding: "2%"})
-            $("#todo").css({position: "absolute", top: "0", left: "0", width: "100%", height: "150%", backgroundColor: "rgba(20,20,20,0.5)"})
-            $("button").css({fontSize: "1.2em", cursor: "pointer"})
+                $('#agrupar').after(htmlError);
+                $("#botones").css({
+                    position: "absolute",
+                    top: "2%",
+                    left: "15%",
+                    width: "60%",
+                    marginTop: "0",
+                    marginLeft: "0",
+                    fontSize: "1.5em",
+                    border: "1px solid #999999",
+                    backgroundColor: "rgba(200,200,200,0.5)",
+                    padding: "2%"
+                })
+                $("#todo").css({
+                    position: "absolute",
+                    top: "0",
+                    left: "0",
+                    width: "100%",
+                    height: "150%",
+                    backgroundColor: "rgba(20,20,20,0.5)"
+                })
+                $("button").css({fontSize: "1.2em", cursor: "pointer"})
 
-            form.reset();
-        }
+                form.reset();
+            }
 
-    });
+        });
+    }
 
 });
