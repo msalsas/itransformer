d15f9f3d4ecb2748ba70dd74e717ff111c68f0d4
msalsas <manolez@gmail.com>
2020-01-06 12:10:03 +0100

    Remove comments

--- assets/js/efectos_inicio.js
+++ assets/js/efectos_inicio.js
@@ -1,52 +1,24 @@
-/*
- * Itransformer.es is an online application to transform images
-Copyright (C) 2013  Manolo Salsas
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-Contact: manolez@gmail.com - http://msalsas.com
-* */
-
 const $ = require('jquery');
 
 $(function(){ 
 	
-	$("#form").show(500)
-	
-		
-	//Animar input adjuntar imagen:
-	
+	$("#form").show(500);
 
-	animarRojoInput()
+	animarRojoInput();
 
+    function animarRojoInput() {
+        var input = $("input[type='file']")
 
-function animarRojoInput() {
-	var input = $("input[type='file']")
-		
-			input.animate({
-			"borderColor": "#FF0000"
-			}, 1500)	
-			
-			.animate({
-			"borderColor": "#FFFFFF"
-			}, 1500)
-			.queue(function(sig){
-                    animarRojoInput();
-                    sig();
-            })
-		
-}
+        input.animate({
+            "borderColor": "#FF0000"
+        }, 1500)
 
-	
+        .animate({
+            "borderColor": "#FFFFFF"
+        }, 1500)
+        .queue(function(next){
+                animarRojoInput();
+                next();
+        });
+    }
 });
--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,37 +1,16 @@
-/*
- * Itransformer.es is an online application to transform images
-Copyright (C) 2013  Manolo Salsas
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-Contact: manolez@gmail.com - http://msalsas.com
-* */
-
-//muestra/oculta entrada
+//show/hide entry
 (function($) {
     jQuery.fn.mostrarOcultar = function(){
 			
-        this.each(function(){
-	   
-		var elem = $(this);
-	   
-		if(elem.is(":checked")) {
-			elem.nextAll().show();
-		} else {
-			elem.nextAll().hide();		
-		}
-		})
+        this.each(function() {
+            var elem = $(this);
+
+            if(elem.is(":checked")) {
+                elem.nextAll().show();
+            } else {
+                elem.nextAll().hide();
+            }
+		});
 	   return this;
     };
 })(jQuery);
